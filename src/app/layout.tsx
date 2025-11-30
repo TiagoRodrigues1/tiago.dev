5c650c8b0c972fa7ce2062c47fa6dc51d7ba5c83
TiagoRodrigues1 <tiaagorodrigues1999@gmail.com>
2025-11-30 16:29:08 +0000

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { SidebarProvider } from "@/components/ui/sidebar";
@@ -37,6 +38,7 @@ export default function RootLayout({
           <AppSidebar />
           <main className="flex-1 h-full min-h-screen ">{children}</main>
         </SidebarProvider>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
